a6edd94796bcedaee43e3c848ca6861f7dd857af
Francis <francis@livescale.tv>
2021-08-26 15:24:18 -0400

    changing SamplePaymentGateway to PaymentGateway

--- Test/Unit/Gateway/Http/TransferFactoryTest.php
+++ Test/Unit/Gateway/Http/TransferFactoryTest.php
@@ -7,8 +7,8 @@ namespace Magento\SamplePaymentProvider\Test\Unit\Gateway\Http;
 
 use Magento\Payment\Gateway\Http\TransferBuilder;
 use Magento\Payment\Gateway\Http\TransferInterface;
-use Magento\SamplePaymentGateway\Gateway\Http\TransferFactory;
-use Magento\SamplePaymentGateway\Gateway\Request\MockDataRequest;
+use Magento\PaymentGateway\Gateway\Http\TransferFactory;
+use Magento\PaymentGateway\Gateway\Request\MockDataRequest;
 
 class TransferFactoryTest extends \PHPUnit_Framework_TestCase
 {
