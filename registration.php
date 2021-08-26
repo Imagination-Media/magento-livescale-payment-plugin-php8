077d6843d7d2d2d08c4eed6da35e492784db3cfe
Francis <francis@livescale.tv>
2021-08-26 15:09:15 -0400

    change composer require

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Livescale_Payment',
+    'Livescale_PaymentGateway',
     __DIR__
 );
