56aa137f9bb4f2a1c9c723f2b8ed82ecc45b1d90
Francis <francis@livescale.tv>
2021-08-30 13:27:38 -0400

    Adding AuthorizePayment mutation

---
+++ Model/AuthorizePayment.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Livescale\Payment\Model;
+
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Payment\Model\Method\Logger;
+
+/**
+ * Class AuthorizePayment
+ */
+class AuthorizePayment implements ResolverInterface
+{
+      /**
+     * @var Logger
+     */
+    private $logger;
+
+    /**
+     * @param Logger $logger
+     */
+    public function __construct(
+        Logger $logger,
+        \Magento\Quote\Api\CartRepositoryInterface $quoteRepository
+    ) {
+        $this->logger = $logger;
+        $this->quoteRepository = $quoteRepository;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+      if (empty($args['input']['cart_id'])) {
+        throw new GraphQlInputException(__('Required parameter "cart_id" is missing'));
+      }
+      if (empty($args['input']['gateway_transaction_id'])) {
+        throw new GraphQlInputException(__('Required parameter "gateway_transaction_id" is missing'));
+      }
+      if (empty($args['input']['gateway_name'])) {
+        throw new GraphQlInputException(__('Required parameter "gateway_name" is missing'));
+      }
+
+      $maskedCartId = $args['input']['cart_id'];
+
+      $quote = $this->quoteRepository->get($maskedCartId);
+      $payment = $quote->getPayment();
+      $paymentId = $payment->getId();
+      
+      $this->logger->debug([
+            'paymentId' => $paymentId
+      ]);
+
+      $currentUserId = $context->getUserId();
+
+      if ($currentUserId !== 0) {
+          throw new GraphQlInputException(__('The request is not allowed for logged in customers'));
+      }
+
+      $storeId = (int)$context->getExtensionAttributes()->getStore()->getId();
+      $cart = $this->getCartForUser->execute($maskedCartId, $currentUserId, $storeId);
+
+      return [
+        'cart' => [
+            'model' => $cart,
+        ],
+    ];
+    }
+}
